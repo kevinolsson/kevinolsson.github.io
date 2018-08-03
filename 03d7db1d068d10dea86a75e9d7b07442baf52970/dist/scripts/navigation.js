7c973a84c7114176a8903f1f18a945721beb1b54
Kevin Olsson <kevin.olsson@Kevin-Olsson-Test.local>
2018-08-03 10:29:10 +0200

    something

---
+++ 03d7db1d068d10dea86a75e9d7b07442baf52970/dist/scripts/navigation.js
@@ -0,0 +1,1 @@
+"use strict";function toggleGlobal(){document.getElementById("global").classList.toggle("global--has-global")}function openSupport(){window.open("support.html")}var globalButton=document.getElementById("globalButton"),globalButton2=document.getElementById("globalButton2");globalButton.addEventListener("click",toggleGlobal),globalButton2.addEventListener("click",toggleGlobal);
